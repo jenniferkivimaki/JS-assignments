3c59cab83bb45df9cfd28dff814dc9c7d5ffc111
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-09-12 15:45:49 +0200

    Create app2.js

---
+++ P5operators/js/app2.js
@@ -0,0 +1,22 @@
+let speedInput =72;
+let speedOutput = 0;
+
+speedOutput = speedInput /3.6;
+console.log(speedOutput);
+
+let minutesInput=3;
+let secondsOutput=0;
+secondsOutput=minutesInput* 60;
+console.log(secondsOutput);
+
+let firstNumberInput =11;
+let secondNumberInput =4;
+let numberOutput=firstNumberInput/secondNumberInput;
+console.log(numberOutput);
+
+let remainderOutput = firstNumberInput % secondNumberInput;
+console.log(remainderOutput);
+
+
+//  % of dividing 11 and 4
+// % 11/4
