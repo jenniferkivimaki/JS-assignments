b598ff2e0d12d9a99869d9708ab69b2089086d77
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-09-12 11:42:04 +0200

    färdig med uppg

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -1,4 +1,14 @@
 let numberOfBottles = 10;
 const line1= "green bottles hanging on the wall";
 const line2="and if one green bottle should accidentally fall, there will be";
-console.log();
+
+console.log(numberOfBottles);
+console.log(line1);
+console.log(numberOfBottles);
+console.log(line1);
+console.log(line2);
+// -= sen hur många du vill ta bort
+// -- innan variabeln om du bara vill ta -1
+numberOfBottles -= 10;
+console.log(numberOfBottles);
+console.log(line1);
