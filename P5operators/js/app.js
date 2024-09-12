13ad6bcd9526ee697aab5e28613aebaae1829b62
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-09-12 14:11:01 +0200

    modulo

---
+++ P5operators/js/app.js
@@ -0,0 +1,40 @@
+//answer först
+//let firstnumber =1;
+//let two =2;
+//let seven = 7;
+//let answer= 0;
+
+//answer= firstnumber + two;'
+//answer= firstnumber - two;
+//answer= firstnumber / two;
+//console.log(answer);
+
+let firstnumber =1;
+let two =2;
+let seven =7;
+let answer=0;
+console.log(answer);
+
+answer= firstnumber + two;
+console.log(answer);
+
+answer= firstnumber - two;
+console.log(answer);
+
+answer= firstnumber / two;
+console.log(answer);
+
+
+let gold= 50;
+gold +10;
+console.log(gold);
+gold=gold+10;
+gold +=10;
+console.log(gold);
+gold -=10;
+console.log(gold);
+gold %=2;
+console.log(gold);
+
+// för att räkna ut ngt m så kort kod som möjligt  + / * - innan =
+//
