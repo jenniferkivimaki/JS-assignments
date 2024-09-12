5516af74eacfb2d778a6036f1044dce277dc8594
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-09-12 10:36:00 +0200

    Update app.js

--- P1-PrintingOutput/js/app.js
+++ P1-PrintingOutput/js/app.js
@@ -1,7 +1,31 @@
-console.log('Hel' +
-  'lo Wo' +
-  'rld!');
+console.log('Hello World!');
 console.log('My name is Jennifer');
+
 console.log('first');
 console.log('second');
+console.log('third');
+console.log('fourth');
+console.log('fifth');
 
+console.log('first');
+console.log('second');
+console.log('third');
+console.log('fourth');
+
+
+//här kan jag skriva vad jag vill och ingen se//
+
+let x=10;
+let y=5;
+let sum = x+y;
+console.log(sum);
+
+
+//var, använd > let eller const//
+
+// number och string
+let hoursinclass=5;
+let hoursofsleep ="five"
+
+console.log(hoursinclass);
+console.log(hoursofsleep);
