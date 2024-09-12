b5c82104cd147397c691a1411ddf3c71970e2de4
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-09-12 14:41:44 +0200

    Update app.js

--- P5operators/js/app.js
+++ P5operators/js/app.js
@@ -28,7 +28,7 @@ console.log(answer);
 let gold= 50;
 gold +10;
 console.log(gold);
-gold=gold+10;
+//gold=gold+10;
 gold +=10;
 console.log(gold);
 gold -=10;
@@ -38,3 +38,13 @@ console.log(gold);
 
 // för att räkna ut ngt m så kort kod som möjligt  + / * - innan =
 //
+
+
+let numberofstudents=0;
+//numberofstudents+=1;
+//console.log(numberofstudents);
+// en förkortning av föregående alt   numberofstudents++;   eller numberofstudents--;
+numberofstudents++;
+console.log(numberofstudents);
+++numberofstudents;
+console.log(numberofstudents);
