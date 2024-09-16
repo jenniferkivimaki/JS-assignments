92cebf5c6db14273063c5117532ef18f4e25579d
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-09-16 10:14:20 +0200

    Update app2.js

--- P5operators/js/app2.js
+++ P5operators/js/app2.js
@@ -20,28 +20,29 @@ console.log(remainderOutput);
 let radieInput=2;
 let piInput=Math.PI
 let areaOutput=radieInput*radieInput*piInput;
+let areOfCircle = Math.PI/2;radieInput, 2;
 console.log(areaOutput);
-
 
 let eleven=11;
 let minusOne=-1;
 let negotiation=eleven*minusOne;
-console.log(negotiation);
+console.log(negotiation); //11
 
 let weightInput=70;
 let heightInput=1.82;
 let heigtOutput=heightInput*heightInput;
 let bmiOutput=weightInput / heigtOutput;
 console.log(bmiOutput);
+//kollA MATH.POW
 
 //a2+b2=c2
 
 let katet1=3;
 let katet2 =4;
 let exponent=2;
-let katet11=katet1**exponent;
+let katet11=katet1**2;
 console.log(katet11);
-let katet22=katet2**exponent;
+let katet22=katet2**2;
 console.log(katet22);
 let hypotenuse=katet11+katet22;
 console.log(hypotenuse);
@@ -51,8 +52,3 @@ console.log(hypotnuseOutput);
 let snabb=Math.sqrt(katet1**exponent+katet2**exponent)
 console.log(snabb);
 
-
-
-let seconds=111;
-let minutes=seconds/60;
-console.log(minutes)
