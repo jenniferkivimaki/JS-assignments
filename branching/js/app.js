4bdbb93a8dc3cd0acff3b93b333b2df92b488d5b
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-09-16 13:09:12 +0200

    ben version

--- branching/js/app.js
+++ branching/js/app.js
@@ -1,16 +1,16 @@
-
-const birthYear= 2000;
+const birthYear = 2010;
 const currentYear = 2024;
 let ageOfPerson = currentYear - birthYear;
-//extra rad om man vill se ålderna också
 console.log(ageOfPerson);
 
-//10 or lower
-let isChild= ageOfPerson <=10 ;
-//11 or 17
-let isTeenager=  (ageOfPerson >=11) && (ageOfPerson <=17) ;
-//18 or more
-let isAdult =    ageOfPerson >=18 ;
+// Save to a let variable named isChild, when the age is from 0 till 10.
+let isChild = (ageOfPerson >= 0) && (ageOfPerson <= 10);
+
+// Save to a let variable named isTeenager, when the age is from 11 to 17
+let isTeenager = (ageOfPerson >= 11) && (ageOfPerson <= 17);
+
+// Save to a let variable named isAdult, when the age is 18 or more.
+let isAdult =  ageOfPerson >= 18;
 
 console.log(isChild);
 console.log(isTeenager);
