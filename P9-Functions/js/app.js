65bdda220affdc68bc4134fed8fc12a2e53b4808
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-09-16 14:52:54 +0200

    First assigment

---
+++ P9-Functions/js/app.js
@@ -0,0 +1,40 @@
+/* walk three steps forward.
+then turn around and
+walk three steps forward
+then turn left and
+walk three steps forward.
+then turn left and
+walk one step forward.
+then turn around and
+walk one step forward */
+
+walkThreeSteps();
+turnAround();
+walkThreeSteps();
+turnLeft();
+walkThreeSteps();
+turnRight();
+stepForward();
+turnAround();
+stepForward();
+
+function stepForward() {
+  console.log("Move forward")
+}
+  function turnRight() {
+  console.log("turn right")
+}
+function turnLeft() {
+  console.log("turn left")
+  console.log("turn left")
+  console.log("turn left")
+}
+     function walkThreeSteps(){
+stepForward();
+stepForward();
+stepForward();
+}
+function turnAround(){
+turnRight();
+turnRight();
+}
