9a99f4273da666ca2d9da1442b570649f1e5d370
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-09-17 14:26:36 +0200

    class

---
+++ P13HTMLDOM/js/app.js
@@ -0,0 +1,20 @@
+// refererar text0 till variabeln som heter textfield0"
+const textfield0 = document.getElementById("text0");
+
+textfield0.innerHTML = "Jennifer";
+
+const headerText5 = document.getElementById("header5");
+headerText5.innerHTML = "Min sida! ";
+headerText5.style.color = "red";
+
+
+const button0 = document.getElementById("button0");
+button0.addEventListener("click", () => {
+  button0();
+});
+
+
+const button1 = document.getElementById("button0");
+button0.addEventListener("click", () => {
+  button0();
+});
