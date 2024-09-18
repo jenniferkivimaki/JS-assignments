bd41a35452263a4084220ce41c91f6ed75c54e86
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-09-18 22:44:20 +0200

    lagt till won/loose

--- P13HTMLDOM/js/app.js
+++ P13HTMLDOM/js/app.js
@@ -1,27 +1,39 @@
+document.body.style.backgroundColor = 'lightgray';
+
 const text = document.getElementById('text');
 const button=document.getElementById('button');
 
 
 let score = 0;
 function increaseBy(inputNumber) {
-  return score += inputNumber;
+return score += inputNumber;
 }
 
 function decreaseBy(inputNumber) {
-  return score -= inputNumber;
-}
+ return score -= inputNumber;
+  }
 
-function updateScore() {
-  const textElement = document.getElementById('text');
-  textElement.innerHTML = `Score: ${score}`;
-}
 
-document.getElementById('button').addEventListener('click', () => {
-  increaseBy(1);
-  updateScore();
-});
+  function updateScore() {
+    const textElement = document.getElementById('text');
+    textElement.innerHTML = `Score: ${score}`;
 
-document.getElementById('button1').addEventListener('click', () => {
-  decreaseBy(1);
-  updateScore();
-});
+    if (score >= 10) {
+      textElement.innerHTML += `<br>You won!`;
+    }
+
+    else if (score <= -10) {
+      textElement.innerHTML += `<br>You loose!`;
+    }
+
+  }
+
+    document.getElementById('button').addEventListener('click', () => {
+      increaseBy(1);
+      updateScore();
+    });
+
+    document.getElementById('button1').addEventListener('click', () => {
+      decreaseBy(1);
+      updateScore();
+    });
