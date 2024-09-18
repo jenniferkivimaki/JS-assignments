9970028b2fac4e72c6f96f658242f25b64720753
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-09-18 10:48:33 +0200

    just playing

--- InputAndOutput/js/app.js
+++ InputAndOutput/js/app.js
@@ -1,4 +1,3 @@
-/*
 
 function multipleBySelf(numberToMultiply) {
   return numberToMultiply * numberToMultiply;
--- P13HTMLDOM/js/app.js
+++ P13HTMLDOM/js/app.js
@@ -1,12 +1,10 @@
 // refererar text0 till variabeln som heter textfield0"
 const textfield0 = document.getElementById("text0");
-
-textfield0.innerHTML = "Jennifer";
+textfield0.innerHTML = multipleBySelf(8);
 
 const headerText5 = document.getElementById("header5");
 headerText5.innerHTML = "Min sida! ";
 headerText5.style.color = "red";
-
 
 const button0 = document.getElementById("button0");
 button0.addEventListener("click", () => {
@@ -18,3 +16,16 @@ const button1 = document.getElementById("button0");
 button0.addEventListener("click", () => {
   button0();
 });
+
+
+function multipleBySelf(numberToMultiply) {
+  return numberToMultiply * numberToMultiply;
+}
+
+
+let score ;
+//skapa en funktion som ökar numret med 1
+function increaseBy(inputNumber) {
+  return ++inputNumber
+}
+
