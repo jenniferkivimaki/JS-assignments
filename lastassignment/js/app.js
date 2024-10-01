4867b62504955ee5c43abb9458b451be5595c961
jenniferkivimaki <jenkiv469@ihmeducation.se>
2024-10-01 13:08:59 +0200

    fm 1/10

--- lastassignment/js/app.js
+++ lastassignment/js/app.js
@@ -1,5 +1,5 @@
 // A B C D E F
-const gradeIdTextField = document.getElementById("grade");
+/* const gradeIdTextField = document.getElementById("grade");
 
 let grade= 79;
 
@@ -59,3 +59,40 @@ return "Min:" + minumum + " , " + "Max:" + maximum;
 }
 
 getMinMax();
+
+*/
+
+//E7 Random_Password
+// Design a program that generates a random password with 6 characters, digits or symbols
+
+//  _   _   _   _   _   _      (6 siffror/bokstäver eller symboler typ allt på tagnentbordet)
+//  [ det här är en ARRAY som symboliserar av dessa kantiga paranteser]
+// Vi ska alltså skapa ett lösenord (en string, därför har vi tecknen inom ""
+
+
+const possibleCharacters= ["0","1","2","3","4","5","6","7","8","9", "a", "b",
+  "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+  "t", "u", "v", "w", "x", "y", "z" , "!", "@" , "#" , "€" , "%" , "/" , "(" , ")" , "=" , "?"];
+
+// console.log(possibleCharacters.length);
+//Nu vet vi att vi har 46 symboler i vår array
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * possibleCharacters.length) ;
+}
+
+// console.log(getRandomInt());
+//consol.logga varje gång för att testa att funktionen funkar ALLTID innan du går vidare
+// i är bara namnet men är standard, i, j , k men kan likka gärna vara jennifer
+
+
+for (let i = 0; i < 6; i ++) {
+let randomArrayIndex = getRandomInt();
+console.log(possibleCharacters[randomArrayIndex]);
+}
+
+// nu funkar funktionen, den tar ut 6 random "boxar" ur arrayen
+
+//possibleCharacters[2];             hitta värdet av box 2
+//possibleCharacters.indexOf ("a");        för att hitta index-numret av "a/innehållet i indexet"
